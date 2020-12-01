1fbf7d7561bdfb1c71d62115c80a7620bea99a8b
Abdeljalil <abdeljalil.aitetaleb@gmail.com>
2020-12-01 17:54:07 +0100

    updated

--- day1.js
+++ day1.js
@@ -2,14 +2,14 @@
 import fs from 'fs';
 
 
-const input = fs.readFileSync('./input1.txt' , { encoding:'utf8' }).toString().split('\r\n')
+const input = fs.readFileSync('./input1.txt' , { encoding:'utf8' }).toString().split('\r\n').map(num => parseInt(num))
 
 let first = 0
 let second = 0
 for (let i = 0; i < input.length; i++) {
     first = input[i]
     const numero = 2020 - parseInt(first)
-    if(input.includes(`${String(numero)}`)){
+    if(input.includes(numero)){
         second = numero
         console.log("found it")
         break;
@@ -31,7 +31,7 @@ for (let i = 0; i < input.length; i++) {
         second = parseInt(input[j])
         const sum = first + second
         const numero = 2020 - sum
-        if(input.includes(`${String(numero)}`)){
+        if(input.includes(numero)){
             third = numero
             console.log("found it")
             break;
