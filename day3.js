dd7134feadc06647356ff18667f5d18f82ea08dc
Abdeljalil <abdeljalil.aitetaleb@gmail.com>
2020-12-04 20:07:09 +0100

    updated

--- day3.js
+++ day3.js
@@ -1,21 +1,55 @@
 import fs from 'fs';
 
+
 //Part 1
 const input = fs.readFileSync('./input3.txt' , { encoding:'utf8' }).toString().split('\r\n').map(line => line.split(''))
 const lineCount = input.length
-const row = input[0].length
 
-let trees = 0
-let index = 3
-for (let i = 1; i < lineCount; i++) {
-    if(!input[i][index]){
-        index = 0
-    }
-    const pos = input[i][index];
-    index = index * i
-    if(pos === '#' ){
-        trees++;
-    }
+// let trees = 0
+// let index = 3
+// let i = 1
+// while(i < lineCount) {
+//     const pos = input[i][index];
+//     index = index + 3
 
+//     if(index > 30){
+//         index -= 31;
+//     }
+//     if(pos === '#' ){
+//         trees++;
+//     }
+//     i++
+// }
+// console.log("number" , trees)
+
+//Part 1
+
+const slopes = [
+    [1,1],
+    [3,1],
+    [5,1],
+    [7,1],
+    [1,2]
+];
+
+let total = 1
+for (const slop of slopes) {
+    let i = slop[1]
+    let trees = 0
+    let index = slop[0]
+    while(i < lineCount) {
+        const pos = input[i][index];
+        index = index + slop[0]
+    
+        if(index > 30){
+            index -= 31;
+        }
+        if(pos === '#' ){
+            trees++;
+        }
+        i = i + slop[1]
+    }  
+    total =  total * trees
 }
-console.log(row)
+
+console.log("number" , total)
