d165200b393f3024aac17a58e7fe6928ae49f267
Abdeljalil <abdeljalil.aitetaleb@gmail.com>
2020-12-04 20:07:31 +0100

    updated

--- day3.js
+++ day3.js
@@ -5,24 +5,24 @@ import fs from 'fs';
 const input = fs.readFileSync('./input3.txt' , { encoding:'utf8' }).toString().split('\r\n').map(line => line.split(''))
 const lineCount = input.length
 
-// let trees = 0
-// let index = 3
-// let i = 1
-// while(i < lineCount) {
-//     const pos = input[i][index];
-//     index = index + 3
+let trees = 0
+let index = 3
+let i = 1
+while(i < lineCount) {
+    const pos = input[i][index];
+    index = index + 3
 
-//     if(index > 30){
-//         index -= 31;
-//     }
-//     if(pos === '#' ){
-//         trees++;
-//     }
-//     i++
-// }
-// console.log("number" , trees)
+    if(index > 30){
+        index -= 31;
+    }
+    if(pos === '#' ){
+        trees++;
+    }
+    i++
+}
+console.log("number" , trees)
 
-//Part 1
+// Part 2
 
 const slopes = [
     [1,1],
