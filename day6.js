bffa68b3d1f43b231a1afdc27dede1c17f1c235b
Abdeljalil <abdeljalil.aitetaleb@gmail.com>
2020-12-07 16:09:01 +0100

    updated

--- day6.js
+++ day6.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
 let groups  = fs.readFileSync('./input6.txt','utf8')
-                .toString().split(/\n\n/)
+                .toString().split(/\n\s*\n/)
                 .map(grp => grp.split(/\n/).join(""))
-                .map(grp => [...new Set([...grp.split("")])])
+                .map(grp => [...new Set([...grp.split("").join("").replace(/\r/g, "")])])
 
 
 const total =  groups.reduce(function(charCount, item) {
@@ -15,11 +15,19 @@ console.log(total);
 
 // Part 2
 groups  = fs.readFileSync('./input6.txt','utf8')
-                .toString().split(/\n\n/).map(grp => grp.split(/\n/))
-console.log(groups[0])
-
+                .toString().split(/\n\s*\n/)
+                .map(grp => grp.split(/\n/).map(str => str.replace(/\r/g, "")).map(str => str.split('').sort()))
+let total2 = 0;
+let answers = new Set()
 for (const grp of groups) {
-    for (const q of grp) {
-        
+  for (const g of grp) {
+    for (const an of g) {
+      if(grp.every(g => g.includes(an))){
+        answers.add(an)
+      }
     }
+  }
+  total2+=answers.size
+  answers = new Set()
 }
+console.log(total2)
